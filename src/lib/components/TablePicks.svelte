60eb91aa92f751b7556c5c43f7adc211c98aefa7
Myles <utmaddog@gmail.com>
2023-09-06 10:03:14 -0500

    support results table name changes

--- src/lib/components/TablePicks.svelte
+++ src/lib/components/TablePicks.svelte
@@ -33,6 +33,84 @@
 								<th>Kick Off</th>
 								<th>League</th>
 								<th>Results</th>
+								<th>Final Score</th>
+							</tr>
+						</thead>
+						<tbody>
+							{#each picks as pick}
+								{#if pick.week === week}
+									<tr>
+										<td>
+											{pick.team_selected.toUpperCase()}
+										</td>
+										{#if pick.team_selected == 'home'}
+											<td>
+												{pick.expand.gameID.home_spread}
+											</td>
+											<td class="text-primary-800 font-medium">
+												{pick.expand.gameID.home_name}
+											</td>
+											<td>
+												{pick.expand.gameID.away_name}
+											</td>
+										{:else}
+											<td>
+												{pick.expand.gameID.away_spread}
+											</td>
+											<td>
+												{pick.expand.gameID.home_name}
+											</td>
+											<td class="text-primary-800 font-medium">
+												{pick.expand.gameID.away_name}
+											</td>
+										{/if}
+										{#if pick.expand.gameID.status === 'FINAL'}
+											<td>FINAL</td>
+											<td>{pick.expand.gameID.league}</td>
+											<td>
+												{pick.results_text}
+											</td>
+											<td>
+												{pick.expand.gameID.home_score} - {pick.expand.gameID.away_score}
+											</td>
+										{:else}
+											<td>{formatDate(pick.expand.gameID.date)} ET</td>
+											<td>{pick.expand.gameID.league}</td>
+											<td />
+											<td />
+										{/if}
+									</tr>
+								{/if}
+							{/each}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</svelte:fragment>
+	</AccordionItem>
+{:else}
+	<AccordionItem>
+		<svelte:fragment slot="summary">
+			<div class="flex grow font-medium text-md">
+				Week {week}
+			</div></svelte:fragment
+		>
+		<svelte:fragment slot="content">
+			<div class="flex max-w-4xl mx-auto">
+				<!-- Responsive Container (recommended) -->
+				<div class="table-container">
+					<!-- Native Table Element -->
+					<table class="table table-hover">
+						<thead>
+							<tr>
+								<th>Team Selected</th>
+								<th>Spread</th>
+								<th>Home Team</th>
+								<th>Away Team</th>
+								<th>Kick Off</th>
+								<th>League</th>
+								<th>Results</th>
+								<th>Final Score</th>
 							</tr>
 						</thead>
 						<tbody>
@@ -66,74 +144,20 @@
 										<td>{formatDate(pick.expand.gameID.date)} ET</td>
 										<td>{pick.expand.gameID.league}</td>
 										{#if pick.expand.gameID.status === 'FINAL'}
+											<td>FINAL</td>
+											<td>{pick.expand.gameID.league}</td>
 											<td>
-												{pick.results}
+												{pick.results_text}
+											</td>
+											<td>
+												{pick.expand.gameID.home_score} - {pick.expand.gameID.away_score}
 											</td>
 										{:else}
+											<td>{formatDate(pick.expand.gameID.date)} ET</td>
+											<td>{pick.expand.gameID.league}</td>
+											<td />
 											<td />
 										{/if}
-									</tr>
-								{/if}
-							{/each}
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</svelte:fragment>
-	</AccordionItem>
-{:else}
-	<AccordionItem>
-		<svelte:fragment slot="summary">
-			<div class="flex grow font-medium text-md">
-				Week {week}
-			</div></svelte:fragment
-		>
-		<svelte:fragment slot="content">
-			<div class="flex max-w-4xl mx-auto">
-				<!-- Responsive Container (recommended) -->
-				<div class="table-container">
-					<!-- Native Table Element -->
-					<table class="table table-hover">
-						<thead>
-							<tr>
-								<th>Team Selected</th>
-								<th>Spread</th>
-								<th>Home Team</th>
-								<th>Away Team</th>
-								<th>Kick Off</th>
-								<th>League</th>
-							</tr>
-						</thead>
-						<tbody>
-							{#each picks as pick}
-								{#if pick.week === week}
-									<tr>
-										<td>
-											{pick.team_selected.toUpperCase()}
-										</td>
-										{#if pick.team_selected == 'home'}
-											<td>
-												{pick.expand.gameID.home_spread}
-											</td>
-											<td class="text-primary-800 font-medium">
-												{pick.expand.gameID.home_name}
-											</td>
-											<td>
-												{pick.expand.gameID.away_name}
-											</td>
-										{:else}
-											<td>
-												{pick.expand.gameID.away_spread}
-											</td>
-											<td>
-												{pick.expand.gameID.home_name}
-											</td>
-											<td class="text-primary-800 font-medium">
-												{pick.expand.gameID.away_name}
-											</td>
-										{/if}
-										<td>{formatDate(pick.expand.gameID.date)} ET</td>
-										<td>{pick.expand.gameID.league}</td>
 									</tr>
 								{/if}
 							{/each}
