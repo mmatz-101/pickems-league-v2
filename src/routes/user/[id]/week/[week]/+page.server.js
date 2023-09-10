6bd0b48f672ad25b317b259f23005f57e78bef00
Myles <utmaddog@gmail.com>
2023-09-10 12:02:11 -0500

    Load data before displaying

--- src/routes/user/[id]/week/[week]/+page.server.js
+++ src/routes/user/[id]/week/[week]/+page.server.js
@@ -17,6 +17,37 @@ export async function load({ locals, params }) {
     let gamesNCAAArray = structuredClone(gamesNCAA)
     let picksArray = structuredClone(picks)
 
+    // Determined the selected games before rendering the components.
+    for (const pick of picksArray) {
+        const pickId = pick.expand.gameID.id;
+        let pickSelected = ''
+        if (pick.team_selected === "home") {
+            pickSelected = 'home'
+        }
+        // Check if the pick ID matches any ID in gamesNFLArray
+        const matchedNFLGameIndex = gamesNFLArray.findIndex(game => game.id === pickId);
+
+        // Check if the pick ID matches any ID in gamesNCAAArray
+        const matchedNCAAGameIndex = gamesNCAAArray.findIndex(game => game.id === pickId);
+
+        // If a match is found in either array, set 'selected' to true
+        if (matchedNFLGameIndex !== -1) {
+            gamesNFLArray[matchedNFLGameIndex].selected = true;
+            if (pickSelected === "home") {
+                gamesNFLArray[matchedNFLGameIndex].homeTeamSelected = true;
+            } else {
+                gamesNFLArray[matchedNFLGameIndex].awayTeamSelected = true;
+            }
+        }
+        if (matchedNCAAGameIndex !== -1) {
+            gamesNCAAArray[matchedNCAAGameIndex].selected = true;
+            if (pickSelected === "home") {
+                gamesNCAAArray[matchedNCAAGameIndex].homeTeamSelected = true;
+            } else {
+                gamesNCAAArray[matchedNCAAGameIndex].homeTeamSelected = true;
+            }
+        }
+    }
 
     return {
         week: params.week,
