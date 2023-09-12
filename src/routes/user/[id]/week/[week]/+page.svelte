f2a9393008892d2301d2d675442bb056f5e8a0ea
Myles Matz <62492012+mmatz-101@users.noreply.github.com>
2023-09-12 18:27:35 +0000

    For the first week submit up to 8 NCAA picks

--- src/routes/user/[id]/week/[week]/+page.svelte
+++ src/routes/user/[id]/week/[week]/+page.svelte
@@ -40,7 +40,13 @@
 	<div class="flex items-center">
 		{#if data.week == $currentWeek}
 			<a href={`./${data.week}/review`}>
-				{#if (sumNCAAPicks > 0 || sumNFLPicks > 0) & (sumNCAAPicks <= 4 && sumNFLPicks <= 4)}
+				{#if data.week === 1}
+					{#if (sumNCAAPicks > 0 || sumNFLPicks > 0) & (sumNCAAPicks <= 8 && sumNFLPicks <= 4)}
+						<button class="btn variant-filled-success" type="submit">
+							Review Picks <Icon src={ArrowRightCircle} mini class="h-6" />
+						</button>
+					{/if}
+				{:else if (sumNCAAPicks > 0 || sumNFLPicks > 0) & (sumNCAAPicks <= 4 && sumNFLPicks <= 4)}
 					<button class="btn variant-filled-success" type="submit">
 						Review Picks <Icon src={ArrowRightCircle} mini class="h-6" />
 					</button>
