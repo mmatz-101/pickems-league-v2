45596eec1cd1ec0ea64f2aaf94aa0b4d84cfd014
Myles Matz <62492012+mmatz-101@users.noreply.github.com>
2023-09-28 15:43:29 +0000

    changes to add a db-query for all binny buys

--- src/routes/admin/db-query/+page.server.js
+++ src/routes/admin/db-query/+page.server.js
@@ -20,5 +20,64 @@ export const actions = {
 			console.log(err);
 			throw error(500, 'internal server error');
 		}
+	},
+	updateBinnyBuys: async({ locals }) => {
+		try {
+			const picks = await locals.pb.collection('binny_picks').getFullList({
+				expand: 'gameID'
+			});
+			let picksArray = structuredClone(picks);
+			console.log(picksArray.length)
+
+			for (const pick of picksArray) {
+				await updateBinnyPick(pick, locals);
+				console.log(pick);
+			}
+
+			return {
+				binnySuccess: true
+			};
+		} catch (err) {
+			console.log(err);
+			throw error(500, 'internal server error');
+		}
 	}
 };
+
+async function updateBinnyPick(pick, locals) {
+	console.log(pick.expand.gameID);
+	// If a game is complete then updating the Points and Text associated with that information.
+	if (pick.expand.gameID.status === 'FINAL') {
+		if (pick.expand.gameID.pick_winner === 'PUSH') {
+			pick.results_points = 0;
+			pick.results_text = 'PUSH';
+		}
+		else if (pick.team_selected.toUpperCase() === pick.expand.gameID.pick_winner) {
+			pick.results_points = 1;
+			pick.results_text = 'WIN';
+		} else {
+			pick.results_points = -1;
+			pick.results_text = 'LOST';
+		}
+		await locals.pb.collection('binny_picks').update(pick.id, pick);
+	}
+	// Did you the user pick the favorite or the underdog.
+	if ((pick.fav_or_under != 'FAV') & (pick.fav_or_under != 'UND')) {
+		if (pick.team_selected === 'home') {
+			if (pick.expand.gameID.home_spread < 0) {
+				pick.fav_or_under = 'FAV';
+			} else {
+				pick.fav_or_under = 'UND';
+			}
+		} else {
+			if (pick.expand.gameID.away_spread < 0) {
+				pick.fav_or_under = 'FAV';
+			} else {
+				pick.fav_or_under = 'UND';
+			}
+		}
+		await locals.pb.collection('binny_picks').update(pick.id, pick);
+	}
+}
+
+
--- src/routes/admin/db-query/+page.svelte
+++ src/routes/admin/db-query/+page.svelte
@@ -14,4 +14,16 @@
 	{:else}
 	<div class="space-y-5">Failure</div>
 	{/if}
+	<div class="space-y-5">
+		<h2 class="h2">Update All Binnny Buys</h2>
+        <p class="p">Button will update all picks in the Binny Picks table.</p>
+		<form action="?/updateBinnyBuys" method="POST" class="space-y-2" enctype="multipart/form-data">
+			<button type="submit" class="btn variant-filled-primary w-full">Submit</button>
+		</form>
+	</div>
+	{#if data.binnySuccess === true}
+	<div class="space-y-5">Success</div>
+	{:else}
+	<div class="space-y-5">Failure</div>
+	{/if}
 </div>
